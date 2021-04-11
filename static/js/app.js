3e9dc5aca8cf436e79a57d3c44ff68bb2301746b
kellyf22 <73838061+kellyf22@users.noreply.github.com>
2021-04-11 18:07:05 -0400

    include bar charts

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 d3.json("../data/samples.json").then((importedData) => {
     console.log(importedData);
     var names = importedData.names;
-    console.log(names);
+    var samples = importedData.samples;
+    console.log(samples);
 
     var dropdownMenu = d3.select("#selDataset");
 
@@ -11,9 +12,6 @@ d3.json("../data/samples.json").then((importedData) => {
       dd_option.text(name);
     });
 
-    // now I need a function that returns a sample corresponding to the name. So need to look at the value
-    // in the dropdown and find its index in your array names.
-
     // I guess first I'll initialize the page with a default plot having nothing to do with anything
     function init() {
         data = [{
@@ -21,8 +19,45 @@ d3.json("../data/samples.json").then((importedData) => {
         y: [1, 2, 4, 8, 16] }];
     
         Plotly.newPlot("bar", data);
-    }
+    };
     init();
+
+     // now I need a function that returns a sample corresponding to the name. So need to look at the value
+    // in the dropdown and find its index in your array names.
+    function updatePlotly() {
+        // Use D3 to select the dropdown menu
+        var dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        var dataset = dropdownMenu.node().value;
+        console.log(dataset);
+        myIndex = names.indexOf(dataset);
+        var sampleToPlot = samples[myIndex];
+        console.log(sampleToPlot);
+
+        // 5. Create your trace.
+        var trace = {
+            x: sampleToPlot.sample_values,
+            y: sampleToPlot.otu_ids,
+            type: "bar",
+            orientation: 'h'
+        };
+        
+        // 6. Create the data array for our plot
+        var data1 = [trace];
+        
+        // 7. Define our plot layout
+        var layout = {
+            title: "Top Ten (fingers crossed) OTU Ids",
+            xaxis: { title: "Counts" },
+            yaxis: { title: "OTU Id"}
+        };
+        
+        // 8. Plot the chart to a div tag with id "bar-plot"
+        Plotly.newPlot("bar", data1, layout);
+    };
+    // Call updatePlotly() when a change takes place to the DOM
+    d3.selectAll("body").on("change", updatePlotly);
+
 });
 //   // Call updatePlotly() when a change takes place to the DOM
 //   d3.selectAll("#selDataset").on("change", updatePlotly);
