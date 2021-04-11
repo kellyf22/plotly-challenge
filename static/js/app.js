044d32a071ceea318d1d5d171febb7ef0361f588
kellyf22 <73838061+kellyf22@users.noreply.github.com>
2021-04-11 19:50:51 -0400

    add bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -34,26 +34,64 @@ d3.json("../data/samples.json").then((importedData) => {
         var sampleToPlot = samples[myIndex];
         console.log(sampleToPlot);
 
-        // 5. Create your trace.
+        // BAR CHART
+        // Sample counts are sorted in descending order, so pick the first 10 to plot 
+        var tenSampleValues = sampleToPlot.sample_values.slice(0,10);
+        console.log(tenSampleValues);
+        var tenOTUs = sampleToPlot.otu_ids.slice(0,10).map(otu => "OTU Id " + String(otu));
+        console.log(tenOTUs);
+        var tenLabels = sampleToPlot.otu_labels.slice(0,10).map(label => String(label.split(';').slice(-1)));
+        console.log(tenLabels);
+        
+        // Create a trace
         var trace = {
-            x: sampleToPlot.sample_values,
-            y: sampleToPlot.otu_ids,
+            x: tenSampleValues.reverse(),
+            y: tenOTUs.reverse(),
             type: "bar",
-            orientation: 'h'
+            orientation: "h",
+            text: tenLabels
         };
         
-        // 6. Create the data array for our plot
+        // Create the data array for our plot
         var data1 = [trace];
         
-        // 7. Define our plot layout
+        // Define our plot layout
         var layout = {
-            title: "Top Ten (fingers crossed) OTU Ids",
+            title: "Top Ten OTU Ids",
             xaxis: { title: "Counts" },
-            yaxis: { title: "OTU Id"}
+            yaxis: { title: " "}
         };
         
         // 8. Plot the chart to a div tag with id "bar-plot"
         Plotly.newPlot("bar", data1, layout);
+
+        // BUBBLE CHART
+        var allSampleValues = sampleToPlot.sample_values;
+        console.log(allSampleValues);
+        var allOTUs = sampleToPlot.otu_ids;
+        console.log(allOTUs);
+        var allLabels = sampleToPlot.otu_labels.map(label => String(label.split(';').slice(-1)));
+        console.log(allLabels);
+
+        var traceBub = {
+            x: allOTUs,
+            y: allSampleValues,
+            mode: 'markers',
+            marker: {
+                size: allSampleValues.map(value => Math.sqrt(value)*5)
+            }
+        };
+
+        var dataBub = [traceBub];
+
+        var layoutBub = {
+            title: "Bubbles",
+            showlegend: false,
+            height: 600,
+            width: 600
+        };
+
+        Plotly.newPlot('bubble', dataBub, layoutBub)
     };
     // Call updatePlotly() when a change takes place to the DOM
     d3.selectAll("body").on("change", updatePlotly);
@@ -62,30 +100,3 @@ d3.json("../data/samples.json").then((importedData) => {
 //   // Call updatePlotly() when a change takes place to the DOM
 //   d3.selectAll("#selDataset").on("change", updatePlotly);
   
-//   // This function is called when a dropdown menu item is selected
-//   function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-  
-//     // Initialize x and y arrays
-//     var x = [];
-//     var y = [];
-  
-//     if (dataset === 'dataset1') {
-//       x = [1, 2, 3, 4, 5];
-//       y = [1, 2, 4, 8, 16];
-//     }
-  
-//     else if (dataset === 'dataset2') {
-//       x = [10, 20, 30, 40, 50];
-//       y = [1, 10, 100, 1000, 10000];
-//     }
-  
-//     // Note the extra brackets around 'x' and 'y'
-//     Plotly.restyle("plot", "x", [x]);
-//     Plotly.restyle("plot", "y", [y]);
-//   }
-  
-//   init();
